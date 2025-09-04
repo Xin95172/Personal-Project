7628146dba4b5943fe4b9f54d2f63d03461cdf5b
Xin-Quan, Chen <130816438+Xin95172@users.noreply.github.com>
2025-09-04 11:51:08 +0800

    Update naive_bayes.py

--- textual data analysis/algos/naive_bayes.py
+++ textual data analysis/algos/naive_bayes.py
@@ -7,33 +7,44 @@ from sklearn.naive_bayes import MultinomialNB, BernoulliNB, ComplementNB
 NBType = Literal["multinomial", "bernoulli", "complement"]
 NBModels = MultinomialNB | BernoulliNB | ComplementNB
 
-def fit_naive_bayes_classifier(
-    x_train: np.ndarray | scipy.sparse.spmatrix, # Bow 或 TF-IDF，shape = (n_samples, n_features)
-    y_train: np.ndarray, # 標籤向量，長度 = n_samples
-    model: NBType = "multinomial",
-    alpha: float = 1.0, # 平滑參數
-) -> NBModels:
-    """
-    訓練 naive bayes classifier
-    """
-    if model == "multinomial":
-        clf = MultinomialNB(alpha = alpha)
-    elif model == "bernoulli":
-        clf = BernoulliNB(alpha = alpha)
-    elif model == "complement":
-        clf = ComplementNB(alpha = alpha)
+class NaiveBayesClassifier:
+    def __init__(
+            self,
+            model: NBType = "multinomial",                  # "multinomial", "bernoulli", "complement"
+            alpha: float = 1.0                              # 平滑參數
+    ):
+        self.model = model
+        self.alpha = alpha
+        if model == "multinomial":
+            self.clf = MultinomialNB(alpha = alpha)
+        elif model == "bernoulli":
+            self.clf = BernoulliNB(alpha = alpha)
+        elif model == "complement":
+            self.clf = ComplementNB(alpha = alpha)
+        self.is_fitted = False
 
-    clf.fit(x_train, y_train)
+    def fit(
+            self,
+            x_train: np.ndarray | scipy.sparse.spmatrix,    # Bow 或 TF-IDF，shape = (n_samples, n_features)
+            y_train: np.ndarray,                            # 標籤向量，長度 = n_samples
+    ) -> NBModels:
+        """
+        fit the model
+        """
+        self.clf.fit(x_train, y_train)
+        self.is_fitted = True
 
-    return clf
+        return self.clf
 
-def predict_naive_bayes_classifier(
-        clf: NBModels, # 用 fit_naive_nayes_classifier 訓練好的 model
-        x_test: np.ndarray|scipy.sparse.spmatrix # Bow 或 TF-IDF，shape = (n_samples, n_features)
-) -> np.ndarray:
-    """
-    使用 naive bayes classifier 預測
-    """
-    y_pred = clf.predict(x_test)
+    def predict(
+            self,
+            x_test: np.ndarray | scipy.sparse.spmatrix # Bow 或 TF-IDF，shape = (n_samples, n_features)
+    ) -> np.ndarray:
+        """
+        make prediction
+        """
+        if not self.is_fitted:
+            raise RuntimeError("go fitting the model first")
+        y_pred = self.clf.predict(x_test)
 
-    return y_pred
+        return y_pred
